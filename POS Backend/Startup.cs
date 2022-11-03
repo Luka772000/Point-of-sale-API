8f9f45047392b6e3b9b77fa36c013e103ce886fc
Luka772000 <lukabrasnic772000@gmail.com>
2022-11-03 14:50:02 +0100

    novi

--- POS Backend/Startup.cs
+++ POS Backend/Startup.cs
@@ -1,26 +1,17 @@
 using POS_Backend.Extensions;
-using POS_Backend.Interfaces;
-using POS_Backend.Models;
-using POS_Backend.Services;
-using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Diagnostics;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Http.Features;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.FileProviders;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
-using Microsoft.IdentityModel.Tokens;
 using Microsoft.OpenApi.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
+using System.IO;
 using System.Net;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace POS_Backend
 {
@@ -32,24 +23,33 @@ namespace POS_Backend
         }
 
         public IConfiguration Configuration { get; }
-        public object ValidateIssuerSigningKey { get; private set; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+
             services.AddApplicationServices(Configuration);
-            services.AddAutoMapper(typeof(Startup));
-            services.AddControllers().AddNewtonsoftJson(options =>
-            options.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore);
+            services.AddControllers();
+            services.Configure<FormOptions>(o =>
+            {
+                o.ValueLengthLimit = int.MaxValue;
+                o.MultipartBodyLengthLimit = int.MaxValue;
+                o.MemoryBufferThreshold = int.MaxValue;
+            });
             services.AddSwaggerGen(c =>
             {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "PosBackend", Version = "v1" });
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "PosBACKEND", Version = "v1" });
             });
-            services.AddCors(options => {
-                options.AddPolicy("EnableCORS", builder => { builder.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod(); });
+            services.AddCors(options =>
+            {
+                options.AddPolicy("EnableCORS", builder =>
+                {
+                    builder.AllowAnyOrigin()
+                       .AllowAnyHeader()
+                       .AllowAnyMethod();
+                });
             });
             services.AddIdentityServices(Configuration);
-
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -61,9 +61,10 @@ namespace POS_Backend
                 {
                     var logger = appBuilder.ApplicationServices.GetRequiredService<ILogger<Startup>>();
                     var feature = context.Features.Get<IExceptionHandlerFeature>();
+
                     if (feature.Error is not null)
                     {
-                        logger.LogError(feature.Error, "Exception");
+                        logger.LogError(feature.Error, "Exception Here!");
                         context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
                         context.Response.ContentType = "application/json";
                         await context.Response.WriteAsync(System.Text.Json.JsonSerializer.Serialize(new
@@ -78,12 +79,15 @@ namespace POS_Backend
             {
                 app.UseDeveloperExceptionPage();
                 app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "POS v1"));
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "FilmBACKEND v1"));
             }
+            app.UseStaticFiles();
+            
 
             app.UseRouting();
 
-            app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:4200"));
+            app.UseCors(options =>
+            options.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:4200"));
 
             app.UseAuthentication();
 
